5a361f8ca1cd76abdafc04e1bae7e81da0337c9c
LeeBingler <binglerlee@gmail.com>
2023-11-27 16:50:45 +0100

    feat: add CardFilms to the Films page

--- app/(pages)/films/page.tsx
+++ app/(pages)/films/page.tsx
@@ -1,7 +1,19 @@
-import React from 'react';
+import CardFilms from './components/CardFilms';
 
 function Films() {
-    return <div>Films Page</div>;
+    const nameFilms = [
+        "Miss Peregrine's Home for Peculiar Children",
+        'Spirited Away',
+        'The 5th Element',
+        "The Pan's Labyrinth"
+    ];
+    return (
+        <main className='absolute top-0 left-0 flex flex-col'>
+            {nameFilms.map((name, index) => {
+                return <CardFilms key={name} film={name} index={index + 1} />
+            })}
+        </main>
+    );
 }
 
 export default Films;
