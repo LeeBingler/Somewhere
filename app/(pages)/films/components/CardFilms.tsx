c3ae10441d335659a3c305f68a53cc0c63906802
LeeBingler <binglerlee@gmail.com>
2023-11-27 17:25:25 +0100

    feat: add style for the films page

--- app/(pages)/films/components/CardFilms.tsx
+++ app/(pages)/films/components/CardFilms.tsx
@@ -2,14 +2,16 @@ import React from 'react';
 
 function CardFilms({ film, index }: { film: string; index: number }) {
     return (
-        <div className=''>
-            <h1>{film}</h1>
-            <div className='flex items-center'>
-                <p>{index > 10 ? index : '0' + index}</p>
-                <span className='bg-black w-12 h-[2px]'/>
-                <p>more</p>
+        <section className='h-[50vh] relative flex justify-center items-center text-[rgb(37,37,37)] pointer-events-none'>
+            <div className={`absolute pointer-events-auto ${index % 2 ? 'left-[21%]' : 'left-[40%]'}`}>
+                <h1 className='md:text-[max(5vw,90px)]'>{film}</h1>
+                <div className='md:text-[2.5vh] flex items-center gap-7 font-Bon-Voyage italic font-bold'>
+                    <p>{index > 10 ? index : '0' + index}</p>
+                    <span className='bg-black w-12 h-[2px]' />
+                    <p>more</p>
+                </div>
             </div>
-        </div>
+        </section>
     );
 }
 
--- app/(pages)/films/page.tsx
+++ app/(pages)/films/page.tsx
@@ -2,13 +2,13 @@ import CardFilms from './components/CardFilms';
 
 function Films() {
     const nameFilms = [
-        "Miss Peregrine's Home for Peculiar Children",
+        "Miss Peregrine's",
         'Spirited Away',
+        "The Pan's Labyrinth",
         'The 5th Element',
-        "The Pan's Labyrinth"
     ];
     return (
-        <main className='absolute top-0 left-0 flex flex-col'>
+        <main className='flex flex-col'>
             {nameFilms.map((name, index) => {
                 return <CardFilms key={name} film={name} index={index + 1} />
             })}
