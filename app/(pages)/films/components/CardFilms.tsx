2a6e4629bbc08c97e592e367d52ed6f66ca72762
LeeBingler <binglerlee@gmail.com>
2023-11-28 17:32:09 +0100

    feat: animation invert on videoFilm

--- app/(pages)/films/components/CardFilms.tsx
+++ app/(pages)/films/components/CardFilms.tsx
@@ -5,7 +5,7 @@ import VideoFilm from './VideoFilm';
 function CardFilms({ film, index }: { film: string; index: number }) {
     return (
         <section className='h-[50vh] relative flex justify-center items-center text-[rgb(37,37,37)] pointer-events-none group/cardFilm'>
-            <div className={`md:absolute cursor-pointer pointer-events-auto ${index % 2 ? 'left-[21%]' : 'left-[50%]'}`}>
+            <div className={`md:absolute z-10 cursor-pointer pointer-events-auto ${index % 2 ? 'left-[21%]' : 'left-[50%]'}`}>
                 <FilmName film={film} />
                 <div className='absolute md:text-[2.5vh] mt-2 flex items-center gap-x-7 font-Arapey italic'>
                     <p>{index > 10 ? index : '0' + index}</p>
--- app/(pages)/films/components/VideoFilm.tsx
+++ app/(pages)/films/components/VideoFilm.tsx
@@ -2,8 +2,12 @@ import React from 'react';
 
 function VideoFilm({source}: {source: string}) {
     return (
-        <div className='absolute -z-[1] bottom-1/2 h-[0%] group-hover/cardFilm:bottom-[0%] group-hover/cardFilm:h-full w-full transition-all duration-500'>
-            <video className='h-full w-full object-cover' src={source} autoPlay muted loop></video>
+        <div className='absolute bottom-1/2 h-[0%]
+        group-hover/cardFilm:bottom-[0%] group-hover/cardFilm:h-full
+        w-full transition-all duration-500
+        after:z-50 after:w-full after:h-full after:absolute after:top-0 after:backdrop-invert
+        '>
+            <video className='h-full w-full object-cover invert' src={source} autoPlay muted loop></video>
         </div>
     );
 }
