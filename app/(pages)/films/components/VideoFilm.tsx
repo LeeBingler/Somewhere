9679f12ada9f7a19ba09b26c59129fecd2747b80
LeeBingler <binglerlee@gmail.com>
2023-11-29 11:38:08 +0100

    feat: the VideoFilm component don't need a source

--- app/(pages)/films/components/VideoFilm.tsx
+++ app/(pages)/films/components/VideoFilm.tsx
@@ -1,13 +1,26 @@
 import React from 'react';
+import strToCamelCase from '@/app/lib/strToCamelCase';
 
-function VideoFilm({source}: {source: string}) {
+function VideoFilm({ name }: { name: string }) {
+    const dirVideo = '/Videos/Films/';
+    const nameCamelCase = strToCamelCase(name.toLocaleLowerCase());
+    const fileExtension = '.mp4';
+
     return (
-        <div className='absolute bottom-1/2 h-[0%]
+        <div
+            className='hidden md:block absolute bottom-1/2 h-[0%]
         group-hover/cardFilm:bottom-[0%] group-hover/cardFilm:h-full
         w-full transition-all duration-500
         after:z-50 after:w-full after:h-full after:absolute after:top-0 after:backdrop-invert
-        '>
-            <video className='h-full w-full object-cover invert object-[50%30%]' src={source} autoPlay muted loop></video>
+        '
+        >
+            <video
+                className='h-full w-full object-cover invert object-[50%30%]'
+                src={dirVideo + nameCamelCase + fileExtension}
+                autoPlay
+                muted
+                loop
+            ></video>
         </div>
     );
 }
