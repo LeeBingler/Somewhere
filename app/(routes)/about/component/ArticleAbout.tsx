429f77e4e971b3ece1cf1050ee6616238bd41af3
LeeBingler <binglerlee@gmail.com>
2023-12-01 11:54:35 +0100

    feat: add ArticleAabout

---
+++ app/(routes)/about/component/ArticleAbout.tsx
@@ -0,0 +1,48 @@
+import React from 'react';
+
+function ArticleAbout() {
+    return (
+        <article
+            className='w-[70%] h-[80vh] mr-6 overflow-y-scroll text-white whitespace-pre-line
+                        backdrop-blur-sm
+                                    font-Bon-Voyage font-medium text-[6.7vw] leading-[8vw] md:text-5xl
+                                    selection:bg-none selection:text-[rgb(205,0,0)]'
+        >
+            <div>
+                <p>
+                    <b className='font-Poppins font-extrabold'>SOMEWHERE </b> is
+                </p>
+                <p>an webapp that showcase some</p>
+                <p>of Tim Burton's movies,</p>
+                <p>coded by</p>
+                <p>Lee Bingler</p>
+                <p>which focuses on</p>
+                <p>
+                    <b className='font-Poppins font-extrabold'> MAKING CREATIVE</b>
+                </p>
+                <p>
+                    <b className='font-Poppins font-extrabold'> AND USER FRIENDLY EXPERIENCE</b>
+                </p>
+            </div>
+            <div className='mt-10'>
+                <p>
+                    <b className='font-Poppins font-extrabold'> Somewhere </b> is made with
+                </p>
+                <p>a modern tech stack such as,</p>
+                <b className='font-Poppins font-extrabold'> GSAP </b>,
+                <b className='font-Poppins font-extrabold'> TAILWIND </b>,
+                <b className='font-Poppins font-extrabold'> JEST </b>
+                <p>
+                    and
+                    <b className='font-Poppins font-extrabold'> NEXT.JS </b>
+                </p>
+                <p>which bring the greatest</p>
+                <p>user experience with quality animation,</p>
+                <p>mobile and desktop support and a good</p>
+                <p>reliability.</p>
+            </div>
+        </article>
+    );
+}
+
+export default ArticleAbout;
--- app/(routes)/about/page.tsx
+++ app/(routes)/about/page.tsx
@@ -1,23 +1,22 @@
 import React from 'react';
 import { Metadata } from 'next';
-import Link from 'next/link';
 import LogoLink from './component/LogoLink';
+import ArticleAbout from './component/ArticleAbout';
 
 export const metadata: Metadata = {
     title: 'Somewhere - About',
-    description: ''
+    description:
+        "Somewhere is an webapp that showcase Tim Burton's movies, made by Lee Bingler which focuses on making creative and user friendly experience"
 };
 
 export default function About() {
     return (
-        <main className='relative h-screen w-screen bg-WillyWonka bg-cover'>
+        <main className='relative h-[120vh] w-screen bg-WillyWonka bg-fill bg-center'>
             <aside className='text-white w-fit mx-10 pt-10 mb-24 relative z-30'>
-              <LogoLink />
+                <LogoLink />
             </aside>
-            <section className='w-full flex justify-end'>
-              <div className='w-[70%] h-[80vh] mr-6 overflow-y-scroll text-white'>
-                bcp de text
-              </div>
+            <section className='w-full flex justify-center md:justify-end'>
+                <ArticleAbout />
             </section>
         </main>
     );
