51dfb9e596eee632a3985b3c8ec1adcbe676ef39
LeeBingler <binglerlee@gmail.com>
2023-12-06 16:55:42 +0100

    feat: add data nightmare before christmas

--- app/api/films/nightmare-before-christmas/route.ts
+++ app/api/films/nightmare-before-christmas/route.ts
@@ -6,19 +6,35 @@ export async function GET() {
         name: 'Nightmare before christmas',
         displayedName: 'Nightmare Before CHRISTMAS',
         releaseDate: 'October 29, 1993',
-        distributor: '',
-        worldPremiere: '',
-        descriptionText: ['', ''],
-        quotes: [
-            { quote: '', author: '' },
-            { quote: '', author: '' }
+        distributor: 'Buena Vista Picture Distribution',
+        worldPremiere: 'the New York Film Festival',
+        descriptionText: [
+            'It tells the story of Jack Skellington, the King of "Halloween Town", ',
+            'who stumbles upon "Christmas Town" and schemes to take over the holiday.'
         ],
-        awards: ['', '', ''],
+        quotes: [
+            { quote: 'The Nightmare Before Christmas is a visual splendor.', author: 'Reel Views' },
+            { quote: 'Energy and art abound everywhere', author: 'Empire Online' }
+        ],
+        awards: ['Annie Awards', 'Golden Globes', '20/20 Awards'],
         press: [
-            { quote: '', webpage: '' },
-            { quote: '', webpage: '' },
-            { quote: '', webpage: '' },
-            { quote: '', webpage: '' }
+            {
+                name: 'Roger Elbert',
+                quote: 'Everything looks strange and haunting. Even Santa Claus would be difficult to recognize without his red-and-white uniform.',
+                webpage:
+                    'https://www.rogerebert.com/reviews/tim-burtons-the-nightmare-before-christmas-1993'
+            },
+            {
+                name: 'Rolling Stone',
+                quote: 'This dazzling mix of fun and fright also explodes the notion that animation is kid stuff.',
+                webpage:
+                    'https://www.rollingstone.com/tv-movies/tv-movie-reviews/tim-burtons-the-nightmare-before-christmas-118171/'
+            },
+            {
+                name: 'Rotten Tomatoes',
+                quote: 'The Nightmare Before Christmas is a stunningly original and visually delightful work of stop-motion animation.',
+                webpage: 'https://www.rottentomatoes.com/m/nightmare_before_christmas'
+            }
         ]
     };
 
