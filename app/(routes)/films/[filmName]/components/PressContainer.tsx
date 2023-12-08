032f4542e62bdb5dbb6b72dec68f617aabbea8fb
LeeBingler <binglerlee@gmail.com>
2023-12-08 11:54:15 +0100

    fix: lint error

--- app/(routes)/films/[filmName]/components/PressContainer.tsx
+++ app/(routes)/films/[filmName]/components/PressContainer.tsx
@@ -5,7 +5,7 @@ function PressContainer({ press }: { press: { name: string; quote: string; webpa
     return (
         <div className='flex flex-col justify-between py-7 pr-10 gap-y-10 w-full'>
             <h2 className='font-Poppins font-extrabold underline text-[1.2em]'>{press.name}</h2>
-            <p className='font-Arapey whitespace-pre-line my-5'>"{press.quote}"</p>
+            <p className='font-Arapey whitespace-pre-line my-5'>&apos;{press.quote}&apos;</p>
             <Link
                 className='font-Poppins font-extrabold opacity-50 hover:opacity-100 w-fit'
                 href={press.webpage}
